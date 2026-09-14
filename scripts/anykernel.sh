f9a7163aa076a430af3b209b03a37bf05738cded
Faqih Ardian Syah <akufaqih1@gmail.com>
2026-09-15 05:11:06 +0700

    scripts: update anykernel.sh kernel string to Mocchipyon

--- scripts/anykernel.sh
+++ scripts/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Phrolova Kernel — by @Naidrahiqa
+kernel.string=Mocchipyon Kernel — by @Naidrahiqa
 do.devicecheck=0
 do.modules=0
 do.systemless=1

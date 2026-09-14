c1f2711c79821e1fad8c54f4aed11673b3408b06
Faqih Ardian Syah <akufaqih1@gmail.com>
2026-09-14 19:16:31 +0700

    ci: use anykernel.sh from scripts/ instead of inline
    Copy anykernel.sh from android_kernel_xiaomi_selene (4.14 project)
    for consistent AK3 packaging across kernel builds.

---
+++ scripts/anykernel.sh
@@ -0,0 +1,40 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Phrolova Kernel — by @Naidrahiqa
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=selene
+device.name2=merlin
+device.name3=lancelot
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+
+## boot shell variables
+block=auto;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot;
+write_boot;
+## end boot install
